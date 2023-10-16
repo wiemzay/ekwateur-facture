e2fa9f1735eb6f6dd2f4b0524ebc608fe19b1a3d
wiem.zayati <wiem.zayati@hydatis.com>
2023-10-16 16:15:16 +0100

    amélioration

--- src/main/java/com/ekwateur/facture/entities/Client.java
+++ src/main/java/com/ekwateur/facture/entities/Client.java
@@ -5,25 +5,7 @@ import java.util.List;
 public class Client {
     private String referenceClient;
     private List<Energie> energies;
-    private double consommationElectricite;
-    private double consommationGaz;
 
-
-    public double getConsommationElectricite() {
-        return consommationElectricite;
-    }
-
-    public void setConsommationElectricite(double consommationElectricite) {
-        this.consommationElectricite = consommationElectricite;
-    }
-
-    public double getConsommationGaz() {
-        return consommationGaz;
-    }
-
-    public void setConsommationGaz(double consommationGaz) {
-        this.consommationGaz = consommationGaz;
-    }
 
     public String getReferenceClient() {
         return referenceClient;
--- src/main/java/com/ekwateur/facture/services/FacturationService.java
+++ src/main/java/com/ekwateur/facture/services/FacturationService.java
@@ -3,18 +3,25 @@ package com.ekwateur.facture.services;
 import com.ekwateur.facture.entities.*;
 
 public class FacturationService {
+    private static final double ELECTRICITE_PARTICULIER_RATE = 0.121;
+    private static final double GAZ_PARTICULIER_RATE = 0.115;
+    private static final double ELECTRICITE_PRO_HIGH_CA_RATE = 0.114;
+    private static final double GAZ_PRO_HIGH_CA_RATE = 0.111;
+    private static final double ELECTRICITE_PRO_LOW_CA_RATE = 0.118;
+    private static final double GAZ_PRO_LOW_CA_RATE = 0.113;
     public double calculerMontantFacture(Client client, int mois, int annee) {
+
 
         double total = 0;
 
         if (client instanceof ClientParticulier) {
             for (Energie energie : client.getEnergies()) {
                 if (energie.getTypeEnergie().equals(TypeEnergie.ELECTRICITE)) {
-                    total += energie.getConsommation() * 0.121;
+                    total += energie.getConsommation() * ELECTRICITE_PARTICULIER_RATE;
 
                 }
                 if (energie.getTypeEnergie().equals(TypeEnergie.GAZ)) {
-                        total += energie.getConsommation() * 0.115;
+                        total += energie.getConsommation() * GAZ_PARTICULIER_RATE;
 
                 }
             }
@@ -25,11 +32,11 @@ public class FacturationService {
 
 
                     if (energie.getTypeEnergie().equals(TypeEnergie.ELECTRICITE)) {
-                        total += energie.getConsommation() * 0.114;
+                        total += energie.getConsommation() * ELECTRICITE_PRO_HIGH_CA_RATE;
 
                     }
                     if (energie.getTypeEnergie().equals(TypeEnergie.GAZ)) {
-                        total += energie.getConsommation() * 0.111;
+                        total += energie.getConsommation() * GAZ_PRO_HIGH_CA_RATE;
 
                     }
                 }
@@ -38,11 +45,11 @@ public class FacturationService {
 
 
                     if (energie.getTypeEnergie().equals(TypeEnergie.ELECTRICITE)) {
-                        total += energie.getConsommation() * 0.118;
+                        total += energie.getConsommation() * ELECTRICITE_PRO_LOW_CA_RATE;
 
                     }
                     if (energie.getTypeEnergie().equals(TypeEnergie.GAZ)) {
-                        total += energie.getConsommation() * 0.113;
+                        total += energie.getConsommation() * GAZ_PRO_LOW_CA_RATE;
 
                     }
                 }
