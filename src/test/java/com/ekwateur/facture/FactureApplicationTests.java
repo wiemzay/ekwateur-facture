ea1fb719d9f37e718d194084e535a48c17a0bfd9
wiem.zayati <wiem.zayati@hydatis.com>
2023-10-16 14:58:16 +0100

    création du projet

---
+++ src/test/java/com/ekwateur/facture/FactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekwateur.facture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
